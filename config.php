0121c1ca3abe4e2d7d981f5ef39ee4a4c5595730
benjaminmarchetti <benjamarchetti4@gmail.com>
2025-06-03 21:31:06 -0300

    Update config.php

--- config.php
+++ config.php
@@ -12,10 +12,10 @@ require_once 'vendor/autoload.php';
 $google_client = new Google_Client();
 
 //Set the OAuth 2.0 Client ID | Copiar "Aqui tu ID DE CLIENTE"
-$google_client->setClientId('548184883948-d0eu42hisua220f08nl9b2vk471lkavi.apps.googleusercontent.com');
+$google_client->setClientId('xxxx-d0eu42hisua220f08nl9b2vk471lkavi.apps.googleusercontent.com');
 
 //Set the OAuth 2.0 Client Secret key Aqui tu CLAVE
-$google_client->setClientSecret('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');
+$google_client->setClientSecret('xxxxX-5moAuI6xFhY5ALzcPIb0OjYhPUV9');
 
 //Set the OAuth 2.0 Redirect URI | URL AUTORIZADO
 $google_client->setRedirectUri('http://localhost/accesocorrecto.php');
